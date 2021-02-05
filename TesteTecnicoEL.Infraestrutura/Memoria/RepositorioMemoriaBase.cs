c262c1087ca7fc78dc261ffa3eac8d8b24612ada
Rafael Brasil <rafael.brasil@localiza.com>
2021-02-05 19:24:50 -0300

    projetos de infra, API e testes

---
+++ TesteTecnicoEL.Infraestrutura/Memoria/RepositorioMemoriaBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TesteTecnicoEL.Dominio;
+
+namespace TesteTecnicoEL.Infraestrutura.Memoria
+{
+    public abstract class RepositorioMemoriaBase<T> : IRepositorioBase<T>
+    {
+        public Task<T> ObterPorId(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
